248bdd90f15e211511baf0edcbdac72cfabb2ad5
Tolebanich <tratatolik1488@gmail.com>
2025-01-14 08:10:52 +0500

    Переделка домашки

--- 08_lesson/DZ/test_projects.py
+++ 08_lesson/DZ/test_projects.py
@@ -1,4 +1,5 @@
 from data.projects_api import projects_api
+import requests
 
 api = projects_api("https://ru.yougile.com/api-v2")
 
@@ -17,11 +18,11 @@ def test_create_proj():
     assert before < after
     assert body['content'][-1]['title'] == "Kadia"
     
-def test_find_by_id():
-    new_proj = api.create_proj("Kadia_id_test") # Создаём новый проект
-    need_id = new_proj['id'] # Сохраняем айди нового проекта
-    comp_by_id = api.search_by_id(need_id) # Ищем по новому айди создался ли проект
-    assert comp_by_id["title"] == "Kadia_id_test"
+# def test_find_by_id():
+#     new_proj = api.create_proj("Kadia_id_test") # Создаём новый проект
+#     need_id = new_proj['id'] # Сохраняем айди нового проекта
+#     comp_by_id = api.search_by_id(need_id) # Ищем по новому айди создался ли проект
+#     assert comp_by_id["title"] == "Kadia_id_test"
     
 def test_edit():
     new_proj = api.create_proj("Kadia_edit_test") # Создаём новый проект
@@ -29,3 +30,26 @@ def test_edit():
     api.change_proj(change_id, "No more Kadia")
     changed = api.search_by_id(change_id)["title"]
     assert changed == "No more Kadia"
+
+
+def test_get_list_neg():
+    proj_id = api.get_proj_list_neg() # Проверка на запрос при отсутствии токена авторизации
+    assert len(proj_id) > 0
+    
+def test_create_proj_neg_no_title():
+    response = api.create_proj("")  # Пустой заголовок
+    assert response["error"], "Ожидалась ошибка при создании проекта без названия"
+
+def test_create_proj_neg():
+    body = api.get_proj_list() # Получаем список проектов
+    before = len(body["content"]) # сохраняем длину
+    api.create_proj_neg_tok("Kadia") #Создать проект без токена
+    body = api.get_proj_list() # Получаем новый список проектов
+    after = len(body["content"]) # Сохраняем новую длину
+    assert before == after # Проверяем что ничего не создалось
+
+def test_find_by_id_neg():
+    api.search_by_id_neg("id") # Ищем по пустому айди
+    
+def test_change_neg():
+    api.change_proj_neg(123, "No more Kadia") # Посылаем запрос на поиск по айди,по неправильному айди
